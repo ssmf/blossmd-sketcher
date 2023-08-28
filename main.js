f09e35f7ae0d7458e222f85d316e6d265135935a
ssmf <chamberkid1@gmail.com>
2023-08-28 17:55:52 +0200

    added functionality and foundational css to buttons

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 const canvas = document.querySelector('.canvas');
-const ifRainbow = false;
+let ifRainbow = true;
+let eraser = false;
+let grid = 16;
 
-const colorButton = document.querySelector('.rainbow');
+const colorButton = document.querySelector('.color');
+const rainbowButton = document.querySelector('.rainbow');
+const eraserButton = document.querySelector('.eraser');
+const clearButton = document.querySelector('.clear');
+const btns = document.querySelectorAll('.btn');
 
-function makeCanvas(grid) {
+colorButton.addEventListener('click', () => {ifRainbow = false; eraser = false; cssButtonToggle(colorButton)})
+rainbowButton.addEventListener('click', () => {ifRainbow = true; eraser = false; cssButtonToggle(rainbowButton)})
+eraserButton.addEventListener('click', () => {eraser = true; cssButtonToggle(eraserButton)})
+clearButton.addEventListener('click',clear)
 
-    for (let i = 0; i < grid; i++) {
+function cssButtonToggle(mainButton) {
+    console.log(mainButton);
+    btns.forEach(btn => btn.classList.remove('activated'));
+    mainButton.classList.add('activated');
+}
+
+function clear() {
+    let columns = document.querySelectorAll('.column');
+    columns.forEach(column => {
+        column.querySelectorAll('.canvasElement').forEach(canvasElement => {
+            canvasElement.style.backgroundColor = 'rgb(255,255,255)'})});
+};
+
+function makeCanvas(gridValue) {
+
+    for (let i = 0; i < gridValue; i++) {
 
         let column = document.createElement('div');
         column.classList.add('column');
         canvas.appendChild(column);
 
-        for (let j = 0; j < grid; j++) {
+        for (let j = 0; j < gridValue; j++) {
             let pixel2 = document.createElement('div');
             pixel2.classList.add('canvasElement');
             column.appendChild(pixel2);
@@ -22,7 +46,15 @@ function makeCanvas(grid) {
 };
 
 function draw() {
-    rainbowDraw(this);
+     if (ifRainbow == true && eraser == false) {
+        rainbowDraw(this);
+     }
+     else if (eraser == true) {
+        erase(this);
+     }
+     else {
+
+     };
 };
 
 function rainbowDraw(element) {
@@ -33,4 +65,12 @@ function colorGenerator() {
     return Math.floor(Math.random() * (255 - 1) + 1);
 };
 
-makeCanvas(16);
+function erase(element) {
+    element.style.backgroundColor = `rgb(255,255,255)`;
+};
+
+function colorDraw() {
+
+};
+
+makeCanvas(grid);

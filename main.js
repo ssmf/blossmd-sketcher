fd2850c226c84ae1fdf50879c70d71a6374aee8b
ssmf <chamberkid1@gmail.com>
2023-08-28 16:51:51 +0200

    Added sketch grid using flexbox

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const canvas = document.querySelector('.canvas')
+const canvas = document.querySelector('.canvas');
+
+function makeCanvas(grid) {
+
+    for (let i = 0; i < grid; i++) {
+
+        let column = document.createElement('div');
+        column.classList.add('column');
+        canvas.appendChild(column);
+
+        for (let j = 0; j < grid; j++) {
+            let pixel2 = document.createElement('div');
+            pixel2.classList.add('canvasElement');
+            column.appendChild(pixel2);
+
+            
+        }
+    };
+};
+
+makeCanvas(16);
